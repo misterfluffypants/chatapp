857fe345d5aeccd9e353b2ae226d5729dc70c156
misterfluffypants <misterpusistyestany@gmail.com>
2025-10-15 09:46:22 +0600

    Fix static path and websocket issues, project now working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
